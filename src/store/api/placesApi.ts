046bbde29fea3d3b866fc01f4f22aeaf21578a2a
Xojiakbar <139034828+Human635@users.noreply.github.com>
2023-10-19 19:57:04 +0500

    null

--- src/store/api/placesApi.ts
+++ src/store/api/placesApi.ts
@@ -1,14 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 import { baseQuery } from "../../utils/baseQuery";
 
-// baseQuery: fetchBaseQuery({
-//   baseUrl: '.....',
-//   prepareHeaders: (headers, { getState }) => {
-//       headers.set('Access-Control-Allow-Origin', '*')
-//       return headers
-//   }
-// }),
-
 export const placesApi = createApi({
   reducerPath: "placesApi",
   baseQuery: fetchBaseQuery({
@@ -17,6 +9,7 @@ export const placesApi = createApi({
       headers.set(
         "X-RapidAPI-Key",
         "ca58d66530msh2cb4599043cf03fp15c0b8jsn6f8360a17224"
+        //e63105d90amsh43a0ca1404c993bp19d9a8jsn92d47c11f9c0
       );
       headers.set("X-RapidAPI-Host", "nomad-list-cities.p.rapidapi.com");
       return headers;
